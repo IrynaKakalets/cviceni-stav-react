c7905b2227c7a48bbdcbfe11a7f17837ef19dc83
Iryna Kakalets <irinakakalec@gmail.com>
2024-05-07 20:31:58 +0200

    Uloha4

--- src/components/Uloha4/index.jsx
+++ src/components/Uloha4/index.jsx
@@ -1,4 +1,6 @@
+import React from 'react';
 import './carousel.css';
+import { useState } from 'react';
 
 /*
 Zadání 1: Nachystejte si adresy obrázků níže do pole.
@@ -20,21 +22,37 @@ Bonus: Pozor na krajní hodnoty. Pokud dojdete na konec nebo začátek pole, tak
   https://source.unsplash.com/7go5UASxmDY/880x500
   https://source.unsplash.com/YmATDIFsCmQ/880x500
 */
+const imageUrl1 = 'https://source.unsplash.com/WLUHO9A_xik/880x500';
+const imageUrl2 = 'https://source.unsplash.com/DA1eGglMmlg/880x500';
+const imageUrl3 = 'https://source.unsplash.com/kTxL6le0Wgk/880x500';
+const imageUrl4 = 'https://source.unsplash.com/7go5UASxmDY/880x500';
+const imageUrl5 = 'https://source.unsplash.com/YmATDIFsCmQ/880x500';
+
+const imageUrls = [imageUrl1, imageUrl2, imageUrl3, imageUrl4, imageUrl5];
 
 export const Uloha4 = () => {
+  const [index, setIndex] = useState(0);
+  const predchozi = () => {
+    setIndex((prevIndex) => (prevIndex === 0 ? imageUrls - 1 : prevIndex - 1));
+  };
+
+  const dalsi = () => {
+    setIndex((prevIndex) => (prevIndex === imageUrls - 1 ? 0 : prevIndex + 1));
+  };
+
   return (
     <div className="carousel">
-      <button className="carousel__predchozi" aria-label="předchozí">
+      <button
+        className="carousel__predchozi"
+        aria-label="předchozí"
+        onClick={predchozi}
+      >
         ←
       </button>
       <div className="carousel__media">
-        <img
-          className="carousel__image"
-          src="https://source.unsplash.com/7go5UASxmDY/880x500"
-          alt=""
-        />
+        <img className="carousel__image" src={imageUrls[index]} alt="" />
       </div>
-      <button className="carousel__dalsi" aria-label="další">
+      <button className="carousel__dalsi" aria-label="další" onClick={dalsi}>
         →
       </button>
     </div>

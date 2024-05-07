da5e40f281c0e9029b1d243c3a5bae911e94cbfc
Iryna Kakalets <irinakakalec@gmail.com>
2024-05-07 20:52:21 +0200

    Uloha8

--- src/components/Uloha8/index.jsx
+++ src/components/Uloha8/index.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { useState } from 'react';
 /*
 Zadání 1: Vytvoř stavovou proměnnou s výchozí hodnotou `'možná'`.
 Zadání 2: Proměnnou vypiš místo `ano/ne/možná`.
@@ -5,10 +7,17 @@ Zadání 3: Po kliknutí na tlačítko změň `možná` na `ano`, `ano` na `ne`,
 */
 
 export const Uloha8 = () => {
+  const [stav, setStav] = useState('možná');
+
+  const zmenitStav = () => {
+    setStav(() => (stav === 'možná' ? 'ano' : stav === 'ano' ? 'ne' : 'možná'));
+  };
+
   return (
     <>
-      <h3>Prší v Brně: ano/ne/možná</h3>
-      <button>změnit</button>
+      <h3>Prší v Brně: {stav}</h3>
+      <button onClick={zmenitStav}>změnit</button>
     </>
-  )
-}
+  );
+};
+

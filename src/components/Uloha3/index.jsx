366485d6c1f6a7619abf6711be0996dfff60ab7e
Iryna Kakalets <irinakakalec@gmail.com>
2024-05-07 20:20:54 +0200

    Uloha3

--- src/components/Uloha3/index.jsx
+++ src/components/Uloha3/index.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import dice1Url from "./1.svg";
 import dice2Url from "./2.svg";
 import dice3Url from "./3.svg";
@@ -6,8 +7,6 @@ import dice5Url from "./5.svg";
 import dice6Url from "./6.svg";
 import "./kostka.css";
 
-const diceUrls = [dice1Url, dice2Url, dice3Url, dice4Url, dice5Url, dice6Url];
-
 /*
 Zadání 1: Převeďte `cisloNaKostce` na stavovou proměnnou.
 Zadání 2: Po kliku na tlačítko zvětšete číslo na kostce o jedna. Po šetce by měla následovat jednička.
@@ -15,20 +14,35 @@ Zadání 3: Vypište v textu `<p>`, jaké číslo kostka ukazuje.
 Zadání 4: Vypište v atributu `alt` správné číslo.
 */
 
+const diceUrls = [dice1Url, dice2Url, dice3Url, dice4Url, dice5Url, dice6Url];
+
 export const Uloha3 = () => {
-  const cisloNaKostce = 1; /* jedna až šest */
+  const [cisloNaKostce, setCisloNaKostce] = useState(1);
+  const [cislo, setCislo] = useState(1);
+
+  const dalsiCislo = () => {
+    if (cisloNaKostce === 6) {
+      setCisloNaKostce(1);
+      setCislo(1); // Po dosažení čísla 6 se vrátí zpět na 1
+    } else {
+      setCisloNaKostce(cisloNaKostce + 1);
+      setCislo(cisloNaKostce + 1); // Nastavení hodnoty "cislo" na další číslo
+    }
+  };
 
   return (
-    <>
+    <div className="kostka">
       <img
-        src={diceUrls[cisloNaKostce - 1]} /* indexy se číslují od nuly */
+        src={diceUrls[cisloNaKostce - 1]}
         width={60}
         height={60}
-        alt="Kostka s číslem @TODO"
+        alt={`Kostka s číslem ${cisloNaKostce}`}
         className="kostka__ikona"
       />
-      <p className="kostka__text">Na kostce je číslo @TODO.</p>
-      <button className="kostka__akce">další</button>
-    </>
+      <p className="kostka__text">Na kostce je číslo {cislo}.</p>
+      <button className="kostka__akce" onClick={dalsiCislo}>
+        další
+      </button>
+    </div>
   );
 };
